5e849f1088b7362bc660db6dca0897b14591569c
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-08-27 14:32:43 -0400

    added rock paper scissors

--- main.go
+++ main.go
@@ -27,10 +27,63 @@ func numberGuesser() bool {
 	return state
 }
 
+func rps() (int, string) {
+	var state int
+	var userInput string
+	var choices = [3]string{"rock", "paper", "scissors"}
+
+	var randIdx int = rand.Intn(len(choices))
+	var randChoice string = choices[randIdx]
+
+	fmt.Print("Pick rock, paper, or scissors: ")
+	fmt.Scan(&userInput)
+
+	if userInput == "rock" {
+		if randChoice == "rock" {
+			state = 0
+		} else if randChoice == "paper" {
+			state = -1
+		} else {
+			state = 1
+		}
+	} else if userInput == "paper" {
+		if randChoice == "rock" {
+			state = 1
+		} else if randChoice == "paper" {
+			state = 0
+		} else {
+			state = -1
+		}
+	} else if userInput == "scissors" {
+		if randChoice == "rock" {
+			state = -1
+		} else if randChoice == "paper" {
+			state = 1
+		} else {
+			state = 0
+		}
+	} else {
+		state = -2
+	}
+
+	return state, randChoice
+}
+
 func main() {
 	if numberGuesser() {
 		fmt.Println("Good job! You got it right")
 	} else {
 		fmt.Println("That was wrong. Ahh bummer, maybe next time")
+	}
+
+	ans, choice := rps()
+	if ans == 1 {
+		fmt.Printf("Good job! You won! The computer chose: %v", choice)
+	} else if ans == 0 {
+		fmt.Printf("You tied with the computer. The computer chose: %v", choice)
+	} else if ans == -1 {
+		fmt.Printf("You lost. Ahh bummer, maybe next time. The computer chose: %v", choice)
+	} else {
+		fmt.Println("Error")
 	}
 }
